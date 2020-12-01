e2b98fdd9f9fdff5436bde70814185fb971c0e51
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-01 22:31:15 +0000

    Automatically backed up by Learn

--- linda.rb
+++ linda.rb
@@ -1,13 +1,14 @@
 
-
+status = 'Thank You Mario! But Our Princess Is In Another Castle!'
 
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  
   puts phrase = "It's-a me, Mario!"
 end
 
-mario
 
 def toadstool
   puts status
 end
+
+toadstool
