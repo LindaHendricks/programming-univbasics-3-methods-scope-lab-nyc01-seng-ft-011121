2f2379a23268a2e1c755cf38c247619442cc0f0b
Linda-Monique Nyangoh Timoh Hendricks <linda.timoh@gmail.com>
2020-12-01 22:27:51 +0000

    Automatically backed up by Learn

--- linda.rb
+++ linda.rb
@@ -1,6 +1,6 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase = "It's-a me, Mario!"filter_parameter_logging
+  puts phrase = "It's-a me, Mario!"
 end
 
 mario
